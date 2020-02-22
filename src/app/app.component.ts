8ec2e19677e3a27d85e48f4e57269e43720531ee
alestarbucks <alejandroalvarezco@hotmail.com>
2020-02-22 11:38:13 +0100

    1.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent {
   title = 'engverbs';
 
-  public version:string = "1.0.1";
+  public version:string = "1.0.2";
 
   private verbs:Array<[string, string, string, string]> = [
     ["arise", "arose", "arisen", "levantarse; surgir"],
